0580bfbba7c47bf55877f981250d7c4ca88a98ea
bindupothana <himabindu.pothana@navaratan.com>
2017-03-27 15:54:35 +0530

    Check given number is perfect

--- script/app.js
+++ script/app.js
@@ -1,14 +1,14 @@
 var app=angular.module('perfectApp',[]);
 app.controller('perfectCtrl',function($scope){
 	$scope.perfect=function(num){
-		var i;
-		var sum;
-		 while (i < num) {
-      if (num % i == 0) {
-         sum = sum + i;
-      }
-      i++;
-   }
+		var sum=0;
+		for(var i = 1; i < num; i++)
+        {
+            if(num % i == 0)
+            {
+                sum = sum + i;
+            }
+        }
  
    if (sum == num)
     $scope.result="is a Perfect Number"
